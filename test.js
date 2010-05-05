97be975f1c6ab407170b8e98546984964e0fa51c
Azeroth <matthew.m.mckenzie@gmail.com>
2010-05-05 18:08:56 +1000

    vector3 is alot cleaner now, thanks <micheil>

--- test.js
+++ test.js
@@ -20,9 +20,9 @@ puts('Starting nMath Testing ');
 var vec1 = new nMath.Vector3(12,1,2);
 var vec2 = new nMath.Vector3(1,2,1);
 
-puts("Vector1:"+inspect(vec1));
-puts("Vector2:"+inspect(vec2));
-puts("Vector1.Length:" + vec1.Length());
+puts("vec1:"+inspect(vec1));
+puts("vec2:"+inspect(vec2));
+puts("vec1.Length:" + vec1.Length());
 puts("nMath.Vector3.Add(vec1,vec2): "+inspect(nMath.Vector3.Add(vec1,vec2)));
 puts("nMath.Vector3.Divide(vec1, vec2): "+inspect(nMath.Vector3.Divide(vec1, vec2)));
 puts("nMath.Vector3.DivideByInt(vec1, 2): "+inspect(nMath.Vector3.DivideByInt(vec1, 2)));
