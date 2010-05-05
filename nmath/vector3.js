fbcd1692df7f029286a5ad6922102f1802e18a8b
Azeroth <matthew.m.mckenzie@gmail.com>
2010-05-05 17:38:07 +1000

    vector3 is working

--- nmath/vector3.js
+++ nmath/vector3.js
@@ -1,24 +1,21 @@
 var Vector3 = require('./vector3');
 var nMath = require('../nmath');
 
-nMath.Vector3 = function Vector3()
-{
-	this.X = 0;
-	this.Y = 0;
-	this.Z = 0;
-}
+exports.Vector3 = Vector3;
 
 nMath.Vector3 = function Vector3(x,y,z)
 {
-	this.X = x;
-	this.Y = y;
-	this.Z = z;
+	this.X = x ? x : 0; this.Y = y ? y : 0; this.Z = z? z : 0
+	
 }
-
-
-exports.Vector3 = Vector3;
-
-Vector3.prototype = {
+nMath.Vector3.prototype = {
+	Normalise: function()
+	{
+		var scale = 1.0 / this.Length;
+		this.X *= scale;
+		this.Y *= scale;
+		this.Z *= scale;
+	},
 	Length: function()
 	{
 		return Math.sqrt(this.X * this.X + this.Y * this.Y + this.Z * this.Z);
@@ -26,12 +23,42 @@ Vector3.prototype = {
 	LengthSquared: function()
 	{
 		return this.X * this.X + this.Y * this.Y + this.Z * this.Z; 
-	},
-	Normalise: function()
-	{
-		var scale = 1.0 / this.Length;
-        this.X *= scale;
-        this.Y *= scale;
-        this.Z *= scale;
 	}
+}
+nMath.Vector3.UnitX = new nMath.Vector3(1, 0, 0);
+nMath.Vector3.UnitY = new nMath.Vector3(0, 1, 0);
+nMath.Vector3.UnitZ = new nMath.Vector3(0, 0, 1);
+nMath.Vector3.Zero = new nMath.Vector3(0, 0, 0);
+nMath.Vector3.One = new nMath.Vector3(1, 1, 1);
+
+nMath.Vector3.Add = function(v1,v2)
+	{
+		return new nMath.Vector3(v1.X+v2.X,v1.Y+v2.Y,v1.Z+v2.Z);
+	}
+Vector3.Subtract = function(v1,v2)
+{
+	return new nMath.Vector3(v1.X-v2.X,v1.Y-v2.Y,v1.Z-v2.Z);
+}
+Vector3.Multiply = function(v,scale)
+{
+	return new nMath.Vector3(v.X * scale.X, v.Y * scale.Y, v.Z * scale.Z);
+}
+nMath.Vector3.MultiplyByInt = function(v1,scale)
+{
+	return new nMath.Vector3(v1.X * scale, v1.Y * scale, v1.Z * scale);
+}
+nMath.Vector3.Divide = function(v,scale)
+{
+	return new nMath.Vector3(v.X / scale.X, v.Y / scale.Y, v.Z / scale.Z);
+}
+nMath.Vector3.DivideByInt = function(v1,scale)
+{
+	return nMath.Vector3.MultiplyByInt(v1, 1/scale);
+}
+nMath.Vector3.ComponentMin = function(a, b)
+{
+	a.X = a.X < b.X ? a.X : b.X;
+	a.Y = a.Y < b.Y ? a.Y : b.Y;
+	a.Z = a.Z < b.Z ? a.Z : b.Z;
+	return a;
 }
--- test.js
+++ test.js
@@ -4,6 +4,7 @@
 // v0.1a
 
 var sys = require('sys');
+//var nodeunit = require('nodeunit');
 var nMath = require('./nmath');
 puts = sys.puts;
 inspect = sys.inspect;
@@ -15,8 +16,16 @@ function debug (m) {
 function debugObj (m) {
 	puts(inspect(m));
 }
-
 puts('Starting nMath Testing ');
-var vec = new nMath.Vector3(12,1,2);
-var vec2 = new nMath.Vector3(1,1,2);
-debugObj(vec);
+var vec1 = new nMath.Vector3(12,1,2);
+var vec2 = new nMath.Vector3(1,2,1);
+
+puts("Vector1:"+inspect(vec1));
+puts("Vector2:"+inspect(vec2));
+puts("Vector1.Length:" + vec1.Length());
+puts("nMath.Vector3.Add(vec1,vec2): "+inspect(nMath.Vector3.Add(vec1,vec2)));
+puts("nMath.Vector3.Divide(vec1, vec2): "+inspect(nMath.Vector3.Divide(vec1, vec2)));
+puts("nMath.Vector3.DivideByInt(vec1, 2): "+inspect(nMath.Vector3.DivideByInt(vec1, 2)));
+
+//vec.Normalise();
+//debugObj(vec);
