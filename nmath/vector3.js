48c03869ee466fc7324317424d0277aed393657e
Azeroth <matthew.m.mckenzie@gmail.com>
2010-05-05 18:11:58 +1000

    vector3 is alot cleaner now, thanks <micheil>

--- nmath/vector3.js
+++ nmath/vector3.js
@@ -1,14 +1,19 @@
-var Vector3 = require('./vector3');
+//var Vector3 = require('./vector3');
 var nMath = require('../nmath');
 
 exports.Vector3 = Vector3;
 
-nMath.Vector3 = function Vector3(x,y,z)
+nMath.Vector3 = Vector3;
+
+function Vector3(x,y,z)
 {
-	this.X = x ? x : 0; this.Y = y ? y : 0; this.Z = z? z : 0
-	
+	this.X = x || 0;
+	this.Y = y || 0;
+	this.Z = z || 0;
 }
-nMath.Vector3.prototype = {
+
+
+Vector3.prototype = {
 	Normalise: function()
 	{
 		var scale = 1.0 / this.Length;
@@ -25,40 +30,41 @@ nMath.Vector3.prototype = {
 		return this.X * this.X + this.Y * this.Y + this.Z * this.Z; 
 	}
 }
-nMath.Vector3.UnitX = new nMath.Vector3(1, 0, 0);
-nMath.Vector3.UnitY = new nMath.Vector3(0, 1, 0);
-nMath.Vector3.UnitZ = new nMath.Vector3(0, 0, 1);
-nMath.Vector3.Zero = new nMath.Vector3(0, 0, 0);
-nMath.Vector3.One = new nMath.Vector3(1, 1, 1);
+Vector3.UnitX = new Vector3(1, 0, 0);
+Vector3.UnitY = new Vector3(0, 1, 0);
+Vector3.UnitZ = new Vector3(0, 0, 1);
+Vector3.Zero = new Vector3(0, 0, 0);
+Vector3.One = new Vector3(1, 1, 1);
 
-nMath.Vector3.Add = function(v1,v2)
-	{
-		return new nMath.Vector3(v1.X+v2.X,v1.Y+v2.Y,v1.Z+v2.Z);
-	}
+Vector3.Add = function(v1,v2)
+{
+	return new Vector3(v1.X+v2.X,v1.Y+v2.Y,v1.Z+v2.Z);
+}
 Vector3.Subtract = function(v1,v2)
 {
-	return new nMath.Vector3(v1.X-v2.X,v1.Y-v2.Y,v1.Z-v2.Z);
+	return new Vector3(v1.X-v2.X,v1.Y-v2.Y,v1.Z-v2.Z);
 }
 Vector3.Multiply = function(v,scale)
 {
-	return new nMath.Vector3(v.X * scale.X, v.Y * scale.Y, v.Z * scale.Z);
+	return new Vector3(v.X * scale.X, v.Y * scale.Y, v.Z * scale.Z);
 }
-nMath.Vector3.MultiplyByInt = function(v1,scale)
+Vector3.MultiplyByInt = function(v1,scale)
 {
-	return new nMath.Vector3(v1.X * scale, v1.Y * scale, v1.Z * scale);
+	return new Vector3(v1.X * scale, v1.Y * scale, v1.Z * scale);
 }
-nMath.Vector3.Divide = function(v,scale)
+Vector3.Divide = function(v,scale)
 {
-	return new nMath.Vector3(v.X / scale.X, v.Y / scale.Y, v.Z / scale.Z);
+	return new Vector3(v.X / scale.X, v.Y / scale.Y, v.Z / scale.Z);
 }
-nMath.Vector3.DivideByInt = function(v1,scale)
+Vector3.DivideByInt = function(v1,scale)
 {
-	return nMath.Vector3.MultiplyByInt(v1, 1/scale);
+	return Vector3.MultiplyByInt(v1, 1/scale);
 }
-nMath.Vector3.ComponentMin = function(a, b)
+Vector3.ComponentMin = function(a, b)
 {
 	a.X = a.X < b.X ? a.X : b.X;
 	a.Y = a.Y < b.Y ? a.Y : b.Y;
 	a.Z = a.Z < b.Z ? a.Z : b.Z;
 	return a;
 }
+
